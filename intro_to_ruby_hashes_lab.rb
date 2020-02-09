b0ec5a78c9b50a0e5b7e2c8b6611395e1f6cead6
Sara Khandaker <kttsara@hotmail.com>
2020-02-09 17:58:10 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,13 +1,13 @@
 def new_hash
-  # return an empty hash
+hash={}
 end
 
 def my_hash
-  # return a valid hash with any key/value pair of your choice
+hash={me: "sara"}
 end
 
 def pioneer
-  # return a hash with a key of :name and a corresponding value of 'Grace Hopper'
+ hash={ :name => "Grace Hopper"}
 end
 
 def id_generator
